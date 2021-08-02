47e84d9ccbcea077f2b210da618913718a653e39
Jaideep <jaideepkumarsingh14@gmail.com>
2021-08-02 08:22:53 +0530

    updated test

--- test.cpp
+++ test.cpp
@@ -1,30 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void power(int n,int p){
-    double ans = 1;
-    unsigned powe = abs(p);
-    while(powe>0){
-        if(powe%2==0){
-            n = n*n;
-            powe = powe/2;
-        }
-        else{
-            ans = ans*n;
-            powe--;
-        }
-    }
-    if(p<0){
-        cout<<((double)1/(double)ans)<<endl;
-    }
-    cout<<ans<<endl;
-}
-
 int main(){
-    int n;
-    cin>>n;
-    int p;
-    cin>>p;
-    power(n,p);
+    
     return 0;
 }
